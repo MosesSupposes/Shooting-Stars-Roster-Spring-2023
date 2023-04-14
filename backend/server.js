c57e40cc73feb8d3287df01e8bb32c7d466fc475
MosesSupposes <mosesasamuel633@gmail.com>
2023-04-14 15:12:46 -0500

    add server boilerplate

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,41 @@ const server = express();
 
 server.use(express.json());
 
-server.post('/api/add-teammate/:name', function(req, res, next) {
-  const { name } = request.params;
+server.get('/api/roster', function(req, res, next) {
 
-  res.status(201).send();
 });
 
-server.put('/api/remove-teammate/:name', function(req, res, next) {
+server.post('/api/roster/:name', function(req, res, next) {
   const { name } = request.params;
 
-  res.status(200).send();
+  return res.status(201).send();
 });
 
-server.delete('/api/update-teammate-info/:name', function(req, res, next) {
+server.put('/api/roster/:name', function(req, res, next) {
   const { name } = request.params;
 
-  res.status(200).send();
+  return res.status(200).send();
 });
 
+server.delete('/api/roster/:name', function(req, res, next) {
+  const { name } = request.params;
 
+  return res.status(200).send();
+});
+
+server.use(function notFound(req, res, next) {
+  const error = new Error('Resource not found.');
+  error.status = 404;
+  return next(error);
+})
+
+server.use(function errorHandler(error, req, res, next) {
+  error.status = error.status || 500;
+  error.message = error.message || 'Internal server error.';
+
+  return res
+    .status(err.status)
+    .json({ error: { message: error.message } });
+})
+
+module.exports = server;

d991c88c7c57dced776e89c2e96bc6380bb35bac
MosesSupposes <mosesasamuel633@gmail.com>
2023-04-14 15:25:25 -0500

    implement a model for the roster table

--- backend/persistence/models/roster.js
+++ backend/persistence/models/roster.js
@@ -1,2 +1,37 @@
 const db = require('../dbConfig')
 
+const model = {
+  getFullRoster() {
+    return db('roster');
+  },
+
+  findById(id) {
+    return db('roster').where({ id });
+  },
+
+  findByJersey(jerseyNumber) {
+    return db('roster').where({ jersey: jerseyNumber });
+  },
+
+  findByName(name) {
+    return db('roster').where({ name });
+  },
+
+  async addNewTeammate(teammateInfo) {
+    const [id] = await db('roster').insert(teammateInfo, 'id');
+    return db('roster').where({ id });
+  },
+
+  async updateTeammateInfo(id, newTeammateInfo) {
+    await db('roster').where({ id }).update(newTeammateInfo);
+    return this.findById(id);
+  },
+
+  removeTeammate(id) {
+    return db('resource').where({ id }).delete()
+  }
+
+}
+
+
+module.exports = model;
